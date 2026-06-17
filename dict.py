a597d144a2fc7dda2ae64852f21f6cf92624ed46
Nishidha Malviya <work.nishidha@gmail.com>
2026-06-17 12:47:51 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,8 +1,7 @@
 my_dict = {'a':'1','b':'2'}
 my_dict['c'] = 3
 print("updated dict", my_dict)
-
-
+#createdmydict
 dict_comp = {x: x**2 for x in range(1, 6)}
 print("Dictionary comprehension:", dict_comp)
 
